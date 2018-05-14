3197a3af3b9c42b5fa39e7a0673bdc9bca8ba1be
Ammar Al-Taweel <ammar@bullionbypost.co.uk>
2018-05-14 08:53:27 +0100

    Add migrations

---
+++ products/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.conf.urls import url, include
+
+from products import views
+
+
+urlpatterns = [
+    url(r'^(?P<product>[\w-]+)$', views.product_view, name='view-product'),
+]
